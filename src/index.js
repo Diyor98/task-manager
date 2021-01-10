563902d9bd3282587f8d0cff3d3c078c45902d93
Diyor Mukhamedkhanov <d.mukhamedkhanov98@gmail.com>
2021-01-10 23:46:30 +0500

    JWT example added

--- src/index.js
+++ src/index.js
@@ -19,16 +19,11 @@ app.listen(port, () => {
 
 
 // For practice purposes
-// const bcrypt =  require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-// const myFunction = async () => {
-//     const password = 'Red12345!'
-//     const hashedPassword = await bcrypt.hash(password, 8)
-
-//     console.log(password)
-//     console.log(hashedPassword)
-
-//     const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-//     console.log(isMatch)
-// }
-// myFunction()
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 seconds' })
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
+}
+myFunction()
