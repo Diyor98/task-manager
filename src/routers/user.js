9efc706b18aa4dc28df247ee032f8e869792023a
Diyor Mukhamedkhanov <d.mukhamedkhanov98@gmail.com>
2021-01-10 22:28:42 +0500

    Login added

--- src/routers/user.js
+++ src/routers/user.js
@@ -46,10 +46,15 @@ router.patch('/users/:id', async (req, res) => {
     }
     
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+        const user = await User.findById(req.params.id)
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
         if (!user){
             return res.status(404).send()
         }
+
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
         res.send(user)
     } catch (e) {
         res.status(400).send()
@@ -69,5 +74,13 @@ router.delete('/users/:id', async (req, res) => {
     }
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+         const user = await User.findByCredentials(req.body.email, req.body.password)
+         res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
 module.exports = router
