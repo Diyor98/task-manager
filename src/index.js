7f219bd6b2725135682d392cf9379fb77a2e7fd2
Diyor <d.mukhamedkhanov98@gmail.com>
2021-01-12 16:24:24 +0500

    Upload profile image added

--- src/index.js
+++ src/index.js
@@ -8,17 +8,29 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
 
-// app.use((req, res, next) => { 
-//     res.status(503).send("The website is under maintenance mode. Try again later.")
-// })
+        cb(undefined, true)  
+    }
+})
+// const errorMiddleware = (req, res, next) => {
+//     throw new Error('From my middleware')
+// }
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
 
 app.use(express.json())
 app.use(userRouter)
@@ -28,17 +40,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-// const main = async () => {
-//     // const task = await Task.findById('5ffd267e6cbcfc30703cbfba')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('5ffd25c3149921095820bc00')
-//     await user.populate('tasks').execPopulate()  
-//     console.log(user.tasks)
-// }
-
-// main()
